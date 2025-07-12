408ef15a371c33164b76134ac7885c833f9df4a4
PC <posedgeclock>
2025-07-12 16:36:32 +0800

    Initial commit

---
+++ BackEnd/src/test/java/cs/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
