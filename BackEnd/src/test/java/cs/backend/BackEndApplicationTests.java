84aba11a844c728c05f4e9edda8c7ecda38ae687
PC <posedgeclock>
2025-07-19 10:08:00 +0800

    实现运行时查看DML语言

--- BackEnd/src/test/java/cs/backend/BackEndApplicationTests.java
+++ BackEnd/src/test/java/cs/backend/BackEndApplicationTests.java
@@ -1,6 +1,6 @@
 package cs.backend;
 
-import cs.backend.service.encryptTest;
+//import cs.backend.service.encryptTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
